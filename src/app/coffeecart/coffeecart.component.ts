40fc5f41ae3b04aa59a51aadcd6508a63d58f0c6
Alex Ordynski <Dr.Hikari@gmail.com>
2019-06-30 11:21:17 +0000

    Add Shopping Cart.

--- src/app/coffeecart/coffeecart.component.ts
+++ src/app/coffeecart/coffeecart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AllcoffeeService } from '../allcoffee.service';
 
 @Component({
   selector: 'app-coffeecart',
@@ -8,7 +9,9 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CoffeecartComponent implements OnInit, OnDestroy {
   id: number;
+  subscription;
   sub;
+  currentCount;
   coffees: any [] = [
     {
       id: 1,
@@ -44,17 +47,33 @@ export class CoffeecartComponent implements OnInit, OnDestroy {
   ]
   selectedCoffee = {};
 
-  constructor(private route:ActivatedRoute) { }
+  constructor(
+    private route:ActivatedRoute,
+    private cardService: AllcoffeeService
+  ) { }
 
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
      this.id = params['id'];
      this.selectedCoffee = this.coffees.find((oneCoffee)=> oneCoffee.id == this.id);
      });
+    
+    this.subscription = this.cardService.getCount().subscribe(
+      (res) => {
+        this.currentCount = res.value;
+      },
+      (err) => {
+        console.error(`An error occurred: ${err.message}`);
+      }
+    );
      console.log(this.id);
    }
 
    ngOnDestroy() {
     this.sub.unsubscribe();
+   }
+   
+   addToShoppingCart() {
+     this.cardService.setCount(this.currentCount + 1);
    }
 }
